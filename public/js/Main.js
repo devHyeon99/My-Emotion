c923fe36d1990c43af4b1e0b7189d7902001739c
devHyeon99 <jinung5@gmail.com>
2023-11-05 17:22:17 +0900

    메인 페이지 UI 작업

--- public/js/Main.js
+++ public/js/Main.js
@@ -5,14 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // 로그인한 카카오 정보 Main 페이지에서 불러옴.
 Kakao.init('7168560f2aefd9e7c731e481723fcf25');
+var email = "";
+var name = "";
+
 function getUser() {
     Kakao.API.request({
         url: '/v2/user/me',
         success: function (res) {
             console.log(res);
-            // 이메일, 이름
-            var email = res.kakao_account.email;
-            var name = res.kakao_account.profile.nickname;
+
+            email = res.kakao_account.email;
+            name = res.kakao_account.profile.nickname;
 
             console.log(email, name);
             const userInfoElement = document.getElementById('user-info');
@@ -29,12 +32,63 @@ function logout() {
     fetch(`https://kauth.kakao.com/oauth/logout?client_id=${client_id}&logout_redirect_uri=${logoutRedirectURI}`, {
         method: 'GET'
     })
-    .then(response => response.text())
-    .then(data => {
-        window.location.href = `https://kauth.kakao.com/oauth/logout?client_id=${client_id}&logout_redirect_uri=${logoutRedirectURI}`;
-        console.log(data); // 로그아웃 성공 시, 서버에서 반환한 응답 데이터
-    })
-    .catch(error => {
-        console.error(error);
+        .then(response => response.text())
+        .then(data => {
+            window.location.href = `https://kauth.kakao.com/oauth/logout?client_id=${client_id}&logout_redirect_uri=${logoutRedirectURI}`;
+            console.log(data); // 로그아웃 성공 시, 서버에서 반환한 응답 데이터
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+// GPT, Clova API 응답 부분
+async function generateResponse() {
+    const userDiary = document.getElementById('userDiary').value;
+    const DiaryWrite = document.getElementById('DiaryWrite');
+    const DiaryWriteView = document.getElementById('DiaryWriteView');
+    const DiaryWriteView2 = document.getElementById('DiaryWriteView2');
+
+    const response = await fetch('https://port-0-my-emotion-jvpb2mlogxbfxf.sel5.cloudtype.app/generateResponse', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userDiary })
     });
+
+    if (response.ok) {
+        const data = await response.json();
+        DiaryWrite.style.backgroundColor = '#DCF8C6';
+        DiaryWrite.style.boxShadow = '2px 2px 20px rgba(0, 0, 0, 0.1)';
+        DiaryWrite.textContent = userDiary;
+        DiaryWriteView.style.backgroundColor = '#E5E5EA';
+        DiaryWriteView.style.boxShadow = '2px 2px 20px rgba(0, 0, 0, 0.1)';
+        DiaryWriteView.textContent = data.assistantResponse;
+        DiaryWriteView2.style.backgroundColor = '#E5E5EA';
+        DiaryWriteView2.style.boxShadow = '2px 2px 20px rgba(0, 0, 0, 0.1)';
+        DiaryWriteView2.textContent = data.confidence;
+        document.getElementById('userDiary').value = '';
+    } else {
+        DiaryWriteView.textContent = '오류';
+    }
+}
+
+// UI 컨트롤 부분
+function changeTab(tabId, button) {
+    // 모든 메뉴 버튼의 활성 클래스 제거
+    document.querySelectorAll('.nav-link').forEach(function (tabButton) {
+        tabButton.classList.remove('active');
+    });
+
+    // 클릭된 메뉴 버튼에 활성 클래스 추가
+    button.classList.add('active');
+
+    // 모든 탭 콘텐츠 숨김
+    document.querySelectorAll('.tab-pane').forEach(function (tabPane) {
+        tabPane.classList.remove('show', 'active');
+    });
+
+    // 클릭된 탭 콘텐츠 표시
+    document.querySelector(`#${tabId}`).classList.add('show', 'active');
 }
